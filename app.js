b69f12cad68c2e0188dcb9620a5c746b90f4bc6d
oto-ml <rmunoz1803@alumno.ipn.mx>
2022-04-30 18:45:32 -0500

    Corrigiendo error en get /v1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
 
-app.get('/v1/explorer', (req, res) => {
+app.get('/v1/explorers', (req, res) => {
     console.log('Api Explorers GET ALL request ${new Date ()}') 
     const explorer1 = {id: 1, name: "Otoniel1"}
     const explorer2 = {id: 2, name: "Otoniel2"}
