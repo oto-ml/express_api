9eefd13875eb447d5a1a3728ec943dbe65d60db5
oto-ml <rmunoz1803@alumno.ipn.mx>
2022-04-30 20:21:17 -0500

    Añadiendo endpoint GET mediante ID

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.get('/v1/explorers', (req, res) => {
     res.status(200).json(explorers)
 })
 
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log('Api Explorers GET request ${new Date()}')
+    console.log('Getting explorer with id ${req.params.id}')
+    const explorer = {id: 1, name: "Oto"}
+    res.status(200).json(explorer)
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
