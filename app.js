832eb08cebb50ade7bf668ca0c22b1d6b44cad1f
oto-ml <rmunoz1803@alumno.ipn.mx>
2022-04-30 20:27:39 -0500

    Agregando endpoint POST /v1/explorers

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.get('/v1/explorers/:id', (req, res) => {
     res.status(200).json(explorer)
 })
 
+app.post('/v1/explorers', (req, res) => {
+    console.log('Api Explorers POST request ${new Date()}')
+    const requestBody = req.body 
+    res.status(201).json({message: "Created"}) 
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
