5ced9b368b73d1d6226e3b4923322ec0d9b91783
oto-ml <rmunoz1803@alumno.ipn.mx>
2022-04-30 20:36:55 -0500

    Agregando endpoint DELETE para /v1/explorers

--- app.js
+++ app.js
@@ -36,6 +36,14 @@ app.put('/v1/explorers/:id', (req, res) => {
     res.status(200).json({message: "Updated!"})
 })
 
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log('Api Explorers DELETE request ${new Date()}') 
+    console.log('Delete explorer with id ${req.params.id}')
+    const requestBody = req.body
+    res.status(200).json({message: "Deleted!"})
+})
+
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
